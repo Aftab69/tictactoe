80111f7c59bce727f8068bdff1c4a24e7ed7018f
Aftab69 <aftabhssn3@gmail.com>
2022-05-23 19:48:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import cross from "./images/cross.png";
+import circle from "./images/circle.png";
+import white from "./images/white.jpg"
 
 function App() {
+  const[image,setImage] = useState(white);
+  const[toggleplayer, setToggleplayer] = useState(false);
+  const handleClick = () =>{
+    if(!toggleplayer){
+      setImage(cross);
+      setToggleplayer(true);
+    } else {
+      setImage(circle);
+      setToggleplayer(false);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container1">
+      <div className="container2">
+        <div className="container3">
+          <div className="box" id="box1">
+            <img onClick={handleClick} src={image} alt={image} />
+          </div>
+          <div className="box" id="box2">
+          <img onClick={handleClick} src={image} alt={image} />
+          </div>
+          <div className="box" id="box3">
+          <img onClick={handleClick} src={image} alt={image} />
+          </div>
+          <div className="box" id="box4">
+          <img onClick={handleClick} src={image} alt={image} />
+          </div>
+          <div className="box" id="box5">
+          <img onClick={handleClick} src={image} alt={image} />
+          </div>
+          <div className="box" id="box6">
+          <img onClick={handleClick} src={image} alt={image} />
+          </div>
+          <div className="box" id="box7">
+          <img onClick={handleClick} src={image} alt={image} />
+          </div>
+          <div className="box" id="box8">
+          <img onClick={handleClick} src={image} alt={image} />
+          </div>
+          <div className="box" id="box9">
+          <img onClick={handleClick} src={image} alt={image} />
+          </div>
+        </div>
+      </div>
     </div>
+    </>
   );
 }
 
